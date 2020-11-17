9a79229a9b703a4426e1d2f70b5e281ec67b19b3
Jhorham <jhorham.petit@alumnos.ucn.cl>
2020-11-16 23:31:43 -0300

    fix(ExampleUnitTest): add new library import

--- app/src/test/java/cl/ucn/disc/dsm/jpetit/news/ExampleUnitTest.java
+++ app/src/test/java/cl/ucn/disc/dsm/jpetit/news/ExampleUnitTest.java
@@ -1,8 +1,9 @@
 package cl.ucn.disc.dsm.jpetit.news;
 
-import org.junit.Test;
 
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,7 +12,5 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
-    }
+    public void addition_isCorrect() { assertEquals(4, 2 + 2); }
 }
