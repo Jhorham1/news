f1b7077fe7ca448ad361bb0df880a82980030b0b
Jhorham <jhorham.petit@alumnos.ucn.cl>
2020-12-17 03:31:45 -0300

    feat(MainActivity): add the toolbar, the FastAdapter and Recycler view

--- app/src/main/java/cl/ucn/disc/dsm/jpetit/news/MainActivity.java
+++ app/src/main/java/cl/ucn/disc/dsm/jpetit/news/MainActivity.java
@@ -1,8 +1,6 @@
 /*
  * Copyright (c) 2020 Jhorham Petit-Mostafa,jhorham.petit@alumnos.ucn.cl
  *
- * Copyright <YEAR> <COPYRIGHT HOLDER>
- *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  *  associated documentation files (the "Software"), to deal in the Software without restriction,
  *  including without limitation the rights to use, copy, modify, merge, publish, distribute,
@@ -22,24 +20,78 @@
 package cl.ucn.disc.dsm.jpetit.news;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.mikepenz.fastadapter.FastAdapter;
+import com.mikepenz.fastadapter.adapters.ModelAdapter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+import cl.ucn.disc.dsm.jpetit.news.model.News;
+import cl.ucn.disc.dsm.jpetit.news.model.NewsItem;
+import cl.ucn.disc.dsm.jpetit.news.services.Contracts;
+import cl.ucn.disc.dsm.jpetit.news.services.ContractsImplNewsApi;
+
 /**
  *the main class.
  *
  * @author Jhorham Petit-Mostafa.
  */
-
 public class MainActivity extends AppCompatActivity {
-/**
- * onCreate
- *
- * @param savedInstanceState used to reload the app.
 
- */
+    /**
+     * The Logger.
+     */
+    private static final Logger log = LoggerFactory.getLogger(MainActivity.class);
+
+    /**
+     * onCreate
+     *
+     * @param savedInstanceState used to reload the app.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // The toolbar
+        this.setSupportActionBar(findViewById(R.id.am_t_toolbar));
+
+        // The FastAdapter
+        ModelAdapter<News, NewsItem> newsAdapter = new ModelAdapter<>(NewsItem::new);
+        FastAdapter<NewsItem> fastAdapter = FastAdapter.with(newsAdapter);
+        fastAdapter.withSelectable(false);
+
+        //The Recycler view
+        RecyclerView recyclerView = findViewById(R.id.am_rv_news);
+        recyclerView.setAdapter(fastAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+
+        // Get the news in the background thread
+        AsyncTask.execute(() -> {
+
+            // Using the contracts to get the news ..
+            Contracts contracts = new ContractsImplNewsApi("87059831b6b444c999918bb3477f7917");
+
+            // Get the News fron NewsApi (internet!)
+            List<News> listNews = contracts.retrieveNews(30);
+
+            // Set the adapter!
+            runOnUiThread(() -> {
+                newsAdapter.add(listNews);
+            });
+        });
+
     }
 }
