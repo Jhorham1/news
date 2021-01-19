d142181e520b48f7a48e4ae044db524e542dc9a9
nectorcr <nector.cortes@alumnos.ucn.cl>
2021-01-19 00:42:27 -0300

    feat(mainactivity): Initialize Fresco

--- app/src/main/java/cl/ucn/disc/dsm/jpetit/news/MainActivity.java
+++ app/src/main/java/cl/ucn/disc/dsm/jpetit/news/MainActivity.java
@@ -80,6 +80,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fresco.initialize(this);
         setContentView(R.layout.activity_main);
 
         // The toolbar
